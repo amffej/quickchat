0797780319b8e95447ebe2f62329d6df190abbf8
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-07-15 17:47:38 -0500

    annouce messages on correct Divs

--- static/sockets.js
+++ static/sockets.js
@@ -6,28 +6,48 @@ document.addEventListener('DOMContentLoaded', () => {
     // When connected, configure buttons
     socket.on('connect', () => {
 
-        function sendMessage() {
+        function txMessage() {
             let message = document.querySelector("#message").value;
+            if (message !== "" ){
             socket.emit('submit message', { 'selection': message });
             document.querySelector("#message").value = "";
         }
+        }
         // Detect button press
         document.querySelector("#sendmessage").onclick = () => {
-            sendMessage();
+            txMessage();
         };
         //Detect Enter Key
         document.addEventListener('keypress', () => {
             if (event.key === "Enter") {
-                sendMessage();
+                txMessage();
             }
         });
     });
 
     // Annouce messace to everyone
     socket.on('announce message', data => {
-        const li = document.createElement('li');
-        li.innerHTML = `Message: ${data.selection}`;
-        document.querySelector('#temp').append(li);
+
+        var username = "username";
+
+        function createDiv()
+        {
+            var newDiv = document.createElement("div"); 
+            newDiv.className = "alert alert-primary mt-2 mb-0 publishedmessage"
+            newDiv.innerHTML = `<h6 class="alert-heading">${username}</h6> ${data.selection}`;
+            return newDiv
+        }
+
+        function rxMessage()
+        {
+            var messages = message = document.querySelectorAll(".publishedmessage");
+            var lastmessage = messages[messages.length];
+            var parentDiv = document.getElementById("messagebox");
+            parentDiv.insertBefore(createDiv(), lastmessage); 
+        }
+
+        rxMessage();
+                
     });
 
 });
