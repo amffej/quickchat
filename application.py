ce104034e32bb5e5c77630dba5ac4fa34d2db5ca
Jefferson Maldonado <jeffmaldo27@gmail.com>
2019-07-19 22:59:51 -0500

    added channel refresh and message channel assignment

--- application.py
+++ application.py
@@ -1,30 +1,35 @@
 import os
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, json, jsonify, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = [(1,'Main Channel'),(2,'Side Channel')]
-#allmessages = [(1,'jeff', 'This is a message','9:46 7/18/2019'), (2,'user2', 'this is another message, a much longer message','9:46 7/18/2019'), (1,'user3', 'hey, this is just random text place here','9:46 7/18/2019'),(1,'jeff', 'This is a message','9:46 7/18/2019'), (2,'user2', 'this is another message, a much longer message','9:46 7/18/2019'), (1,'user3', 'hey, this is just random text place here','9:46 7/18/2019')]
+channels = [(1, 'Main Channel'), (2, 'Side Channel')]
+# allmessages = [(1,'jeff', 'This is a message','9:46 7/18/2019'), (2,'user2', 'this is another message, a much longer message','9:46 7/18/2019'), (1,'user3', 'hey, this is just random text place here','9:46 7/18/2019'),(1,'jeff', 'This is a message','9:46 7/18/2019'), (2,'user2', 'this is another message, a much longer message','9:46 7/18/2019'), (1,'user3', 'hey, this is just random text place here','9:46 7/18/2019')]
 allmessages = []
+
 
 @app.route("/getmessages", methods=["GET"])
 def getmessages():
     channel = request.args.get("channel")
     messages = []
-    for a,b,c,d in allmessages:
+    for a, b, c, d in allmessages:
         if (a == int(channel)):
-            messages.append(tuple((a,b,c,d)))  
+            messages.append(tuple((a, b, c, d)))
     json_ouput = jsonify(messages)
     return (json_ouput)
 
 
-@app.route("/getchannels")
+@app.route("/getchannels", methods=["GET", "POST"])
 def getchannels():
-    return (jsonify(channels))
+    json_output = []
+    for a, b in channels:
+        json_output.append({'id': a, 'channel': b})
+    return (jsonify(json_output))
+
 
 @app.route("/createchannel", methods=["GET"])
 def createchannel():
@@ -32,28 +37,31 @@ def createchannel():
     print(channelname)
     lastID = channels[-1][0]
     newID = lastID + 1
-    for a,b in channels:
+    for a, b in channels:
         if(b == channelname):
             return ("Channel Taken")
-    channels.append(tuple((newID,channelname)))
+    channels.append(tuple((newID, channelname)))
     return (f"Channel ID: {newID} Channel name: {channelname}")
+
 
 @app.route("/")
 def index():
-    #return ("OK")
+    # return ("OK")
     return render_template("index.html")
 
-@socketio.on("submit message")
 
+@socketio.on("submit message")
 def vote(data):
-    #PUBLISH PUBLIC MESSAGES
+    # PUBLISH PUBLIC MESSAGES
     message = data["message"]
-    username = data["username"] 
-    #TODO FIX TIME STAMP AND GET CHANNEL INPUT
+    username = data["username"]
+    channel = int(data["channel"])
+    print("received")
+    print(channel)
+    # TODO FIX TIME STAMP AND GET CHANNEL INPUT
     timestamp = "stamphere"
-    channel = 1
     allmessages.append(tuple((channel,username,message,timestamp)))
-    #print(selection);
+    # print(selection);
     emit("announce message", {"username": username, "message": message}, broadcast=True)
     
-    #TODO PUBLISH PRIVATE MESSAGES
+    # TODO PUBLISH PRIVATE MESSAGES
--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-//TODO GENERATE CHANNEL LIST
-//ADD CHANNEL REFRESH BUTTON
-
+    const template = Handlebars.compile(document.querySelector('#channels').innerHTML);
+    localStorage.setItem("channel", 1);
     //Check if a username was created
     if (localStorage.getItem("username") == null) {
         $('#usernameEntry').modal({
@@ -41,7 +40,29 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             }
         }
-        //TODO: CHECK IF USERNAME IS ALREADY TAKEN
+    };
+
+    function getChannels() {
+
+        const Http = new XMLHttpRequest();
+        const url = '/getchannels';
+        Http.open("GET", url);
+        Http.send();
+        Http.onload = () => {
+            const data = JSON.parse(Http.responseText);
+            document.querySelector('#channellist').innerHTML = "";
+            for (var key in data) {
+                const content = template({ 'channelurl': data[key].id, 'channelname': data[key].channel, 'unreadmessages': 0 });
+                document.querySelector('#channellist').innerHTML += content;
+            }
+            document.querySelectorAll('.list-group-item').forEach(button => {
+                button.onclick = () => { 
+                    localStorage.setItem("channel", button.dataset.channel);
+                    //TODO ADD ACTION FOR WHEN CHANNEL GETS CLICKED;
+                };
+            });
+        };
+
     };
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -49,15 +70,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // When connected
     socket.on('connect', () => {
 
-        
         function txMessage() {
             const username = localStorage.getItem("username");
+            const channel = localStorage.getItem("channel");
             let message = document.querySelector("#message").value;
             if (message !== "") {
-                socket.emit('submit message', {"username": username, "message": message});
+                socket.emit('submit message', { "channel": channel, "username": username, "message": message });
                 document.querySelector("#message").value = "";
+
             }
         }
+
+        document.querySelector("#refreshbutton").onclick = () => {
+            getChannels();
+        };
+
         // Detect button press
         document.querySelector("#sendmessage").onclick = () => {
             txMessage();
@@ -69,6 +96,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 $('#myModal').modal()
             }
         });
+
+
     });
 
     // Annouce messace to everyone
@@ -96,5 +125,5 @@ document.addEventListener('DOMContentLoaded', () => {
         rxMessage();
 
     });
-
+    getChannels();
 });
